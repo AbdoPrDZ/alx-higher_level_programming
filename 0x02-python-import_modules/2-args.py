89d191edbbbea160c08bda3c0d3c7ace793779be
AbdoPrDZ <abdopr47@gmail.com>
2023-11-30 10:46:46 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,10 @@ if __name__ == '__main__':
     argc = len(argv)
     if argc == 1:
         print('{} arguments.'.format(0))
-    elif argc == 2:
-        print('{} argument:'.format(1))
     else:
-        print('{} arguments:'.format(argc - 1))
-    for i in range(1, argc):
-        print('{0}: {1}'.format(i, argv[i]))
+        if argc == 2:
+            print('{} argument:'.format(1))
+        else:
+            print('{} arguments:'.format(argc - 1))
+        for i in range(1, argc):
+            print('{0}: {1}'.format(i, argv[i]))
