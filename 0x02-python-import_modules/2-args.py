75a3a97ae8a84e966e832fe1a64004125f95859f
AbdoPrDZ <abdopr47@gmail.com>
2023-11-30 11:10:14 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    argc = len(argv)
-    if argc == 1:
-        print('{} arguments.'.format(0))
+    aLen = len(argv)
+    if aLen == 1:
+        print('{} arguments.'.format(aLen - 1))
     else:
-        if argc == 2:
-            print('{} argument:'.format(1))
+        if aLen == 2:
+            print('{} argument:'.format(aLen - 1))
         else:
-            print('{} arguments:'.format(argc - 1))
-        for pos, item in enumerate(argv):
-            if not (pos == 0):
-                print('{0}: {1}'.format(pos, item))
+            print('{} arguments:'.format(aLen - 1))
+        for i in range(1, aLen):
+            print('{0}: {1}'.format(i, argv[i]))
