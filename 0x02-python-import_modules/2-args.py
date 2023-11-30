c47df7d4f37ddbb69f6033f3b3ba35b663d32dec
AbdoPrDZ <abdopr47@gmail.com>
2023-11-30 10:39:00 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
-    argc = len(sys.argv)
+    from sys import argv
+    argc = len(argv)
     if argc == 1:
-        print('0 arguments.')
+        print('{} arguments.'.format(0))
     else:
-        print('{0} arguments:'.format(argc - 1))
+        print('{} arguments:'.format(argc - 1))
         for i in range(1, argc):
-            print('{0}: {1}'.format(i, sys.argv[i]))
+            print('{0}: {1}'.format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
-    argc = len(sys.argv)
+    from sys import argv
+    argc = len(argv)
     s = 0
     for i in range(1, argc):
-        n = int(sys.argv[i])
+        n = int(argv[i])
         s += n
     print('{}'.format(s))
