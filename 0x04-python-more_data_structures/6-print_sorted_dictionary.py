a585bfcc66126c3fabfc1e6e8ae64297d46bfdb3
AbdoPrDZ <abdopr47@gmail.com>
2023-12-06 20:22:10 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary:dict):
+def print_sorted_dictionary(a_dictionary):
     """
     print_sorted_dictionary - print directory in sort way
     @a_directory: the directory
     """
-    keys = list(a_dictionary.keys())
-    keys.sort()
+    if a_dictionary:
+        keys = list(a_dictionary.keys())
+        keys.sort()
 
-    for key in keys:
-        print('{0}: {1}'.format(key, a_dictionary[key]))
+        for key in keys:
+            print('{0}: {1}'.format(key, a_dictionary[key]))
