3b6fc685d384d1dbef4e58fbdefc4adcd7378d06
AbdoPrDZ <abdopr47@gmail.com>
2024-03-13 15:27:04 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
 
-if (isNaN(size))
+if (isNaN(size)) {
   console.log('Missing size');
-else
+} else {
   for (let r = 0; r < size; r++) {
     let row = '';
-    for (let c = 0; c < size; c++)
+    for (let c = 0; c < size; c++) {
       row += 'X';
+    }
     console.log(row);
   }
+}
